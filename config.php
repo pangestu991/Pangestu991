bc1852942d674f6575b503d6fd4fd19aafb2229e
pangestu991 <ppq.zeeber69@cyber-wizard.com>
2017-04-23 08:53:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+/**
+*
+*		Baca sebelum setting akun 
+*
+* user diisi username bot fb (bisa diisi terserah dengan karakter a-zA-Z0-9_.)
+* name diisi nama bot fb (perhatikan huruf besar kecil, harus sama persis)
+*		email diisi email bot fb (buat login)
+*		pass diisi password bot fb (buat login)
+*
+* 
+*/
+$cf = array(
+'user'=>'1lt.pangestu',
+'name'=>'Ulung Pangestu',
+'email'=>'Pangestuulung901@gmail.com',
+'pass'=>'Pangestu911',
+
+
+
+
+
+
+
+// system setting
+'timezone'=>'Asia/Jakarta',
+'cycle'=>5,
+'memory_limit'=>'4G',
+'max_execution_time'=>false,
+'display_errors'=>true,
+'ignore_usrabrt'=>true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+);
